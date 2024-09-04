87c9d78bee69a2565cca8fc79e25a6dd7f9493cf
ND11111 <160196623+ND11111@users.noreply.github.com>
2024-09-04 15:04:18 -0400

    Registration.php

--- LAMPAPI/Registration.php
+++ LAMPAPI/Registration.php
@@ -1,65 +1,63 @@
 <?php
 
-    $inData = getRequestInfo();
+$inData = getRequestInfo();
 
-    $firstName = $inData["firstName"];
-    $lastName = $inData["lastName"];
-    $username = $inData["username"];
-    $password = $inData["password"];
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$username = $inData["username"];
+$password = $inData["password"];
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error)
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} 
+else 
+{
+    
+    $stmt = $conn->prepare("SELECT * FROM Users WHERE Username=?");
+    $stmt->bind_param("s", $username); 
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $rows = $result->num_rows;
+
+    if ($rows == 0) 
 	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$sql = "SELECT * FROM Users WHERE Login=?";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param("s", $username); 
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$rows = mysqli_num_rows($result);
+        
+        $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Username, Password) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $firstName, $lastName, $username, $password);
+        $stmt->execute();
+        $id = $conn->insert_id;
 
-		if ($rows == 0)
-		{
-			$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Username, Password) VALUES(?,?,?,?)");
-			$stmt->bind_param("ssss", $firstName, $lastName, $username, $password);
-			$stmt->execute();
-			$id = $conn->insert_id;
-			$stmt->close();
-			$conn->close();
-			http_response_code(200);
-			$searchResults .= '{'.'"ID": "'.$id.''.'"}';
+        $stmt->close();
+        $conn->close();
 
-			returnWithInfo($searchResults);
-		} 
-		else {
-			http_response_code(409);
-			returnWithError("Sorry...Username is taken");
-		}
-	}
+        returnWithInfo($firstName, $lastName, $id);
+    } 
+else 
+    {
+        returnWithError("Sorry...Username is taken");
+    }
+}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+function sendResultInfoAsJson($obj) {
+    header('Content-type: application/json');
+    echo $obj;
+}
 
-	function returnWithError( $err )
-	{
-		$retValue = '{"Error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . ']}';
-		sendResultInfoAsJson( $retValue );
-	}
+function returnWithError($err) {
+    $retValue = '{"Error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($firstName, $lastName, $id) {
+    $retValue = '{"ID":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '"}';
+    sendResultInfoAsJson($retValue);
+}
 
 ?>
